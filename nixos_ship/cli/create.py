2564e54cefb32cb8b56f0de1713aefee5fc9a781
Thomas Watson <twatson52@icloud.com>
2022-06-11 16:20:44 -0700

    refactor create handler

--- nixos_ship/cli/create.py
+++ nixos_ship/cli/create.py
@@ -3,6 +3,29 @@ from ..workdir import Workdir
 from .. import git_utils
 from .. import nix_utils
 
+def get_config_names(flake_path):
+    return nix_utils.eval_flake(flake_path,
+        "nixosConfigurations",
+        "builtins.attrNames")
+
+def build_flake_configs(flake_path, config_names):
+    config_paths = {}
+    workdir = flake_path.parent
+
+    for idx, name in enumerate(config_names):
+        # symlink to an ordinal name in case there are any config names which
+        # are troubling for the filesystem
+        config_path = workdir/f"{flake_path.name}_configs"/f"config_{idx}"
+
+        nix_utils.build_flake(flake_path,
+            f"nixosConfigurations.\"{name}\".config.system.build.toplevel",
+            config_path)
+
+        # resolve the symlink to get the actual store path
+        config_paths[name] = config_path.resolve()
+
+    return config_paths
+
 def create_handler(args):
     source_rev = git_utils.get_commit(args.rev)
 
@@ -10,15 +33,7 @@ def create_handler(args):
         flake_path = workdir/"worktree"
         git_utils.create_worktree(flake_path, source_rev)
 
-        # get the list of nixos configuration names
-        conf_names = nix_utils.eval_flake(flake_path,
-            "nixosConfigurations",
-            "builtins.attrNames")
+        config_names = get_config_names(flake_path)
+        config_paths = build_flake_configs(flake_path, config_names)
 
-        for idx, name in enumerate(conf_names):
-            # build each system configuration
-            nix_utils.build_flake(flake_path,
-                f"nixosConfigurations.\"{name}\".config.system.build.toplevel",
-                workdir/f"system_{idx}")
-
-        print(conf_names)
+        print(config_paths)
